6d2e7b0cb402fa730e89247cf289ca03b6227ca8
Harry <harrypc2021@hotmail.com>
2026-04-19 13:17:26 -0500

    corrección

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Estrategia: Cache-First para assets estáticos
    ============================================================ */
 
-const CACHE_NAME = 'intranotas-v9';
+const CACHE_NAME = 'intranotas-v11';
 
 /* Todos los archivos que deben estar disponibles offline */
 const ARCHIVOS_A_CACHEAR = [
