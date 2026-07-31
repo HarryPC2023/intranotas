ed053dc9d41078bd108206997fb83e70f28fcccf
Harry <harrypc2021@hotmail.com>
2026-07-30 20:57:53 -0500

    tonos degradados

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Estrategia: Cache-First para assets estáticos
    ============================================================ */
 
-const CACHE_NAME = 'intranotas-v38';
+const CACHE_NAME = 'intranotas-v39';
 
 /* Todos los archivos que deben estar disponibles offline */
 const ARCHIVOS_A_CACHEAR = [
